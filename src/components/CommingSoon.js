334ac894327fa29fd419978ffd11f4033c93b1a1
akashmadduru <akashmadduru@gmail.com>
2022-03-24 17:43:11 +0530

    fix

--- src/components/CommingSoon.js
+++ src/components/CommingSoon.js
@@ -1,20 +1,14 @@
 import Bar from './wordle/Bar/Bar'
-import ComingSoonPng from '../assets/coming_soon.svg'
-import { useLocation } from 'react-router'
+import ComingSoonPng from '../assets/cs.png'
 
 const ComingSoon = () => {
 
-	const location = useLocation()
-	let message = ""
-	if (location.pathname === "/sc") message = "Scavenger Hunt"
-	if (location.pathname === "/lottery") message = "Lottery"
+
 
 	return <div style={{ minHeight: "100vh", background: "#321E43" }}>
 		<Bar />
-
-		<div style={{ fontSize: "2rem", color: "white", fontWeight: "bold", display: "flex", justifyContent: "center" }}> {message} </div>
 		<div style={{ color: "white", display: "flex", justifyContent: "center", width: "100%" }}>
-			<img width="900" style={{ display: "flex", alignSelf: "center", justifyContent: "center" }} src={ComingSoonPng} alt="coming soon" />
+			<img width="700" style={{ display: "flex", justifyContent: "center" }} src={ComingSoonPng} alt="coming soon" />
 		</div>
 	</div>
 }
