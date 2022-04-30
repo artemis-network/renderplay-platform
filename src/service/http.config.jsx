1dcfc02f98641dfb92f7ac2609a753d6d0424f07
akashmadduru <akashmadduru@gmail.com>
2022-04-30 12:45:42 +0530

    fix

--- src/service/http.config.jsx
+++ src/service/http.config.jsx
@@ -6,16 +6,16 @@ let URL = "";
 if (SERVER === "DEV") URL = "http://192.168.1.14:5000/";
 if (SERVER === "PROD") URL = "https://artemisnetwork.azurewebsites.net/";
 
-// axios.interceptors.request.use(
-//   (config) => {
-//     // const token = localStorage.getItem("accessToken");
-//     // if (token) config.headers["Authorization"] = token;
-//     // config.baseURL = URL;
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
+axios.interceptors.request.use(
+  (config) => {
+    // const token = localStorage.getItem("accessToken");
+    // if (token) config.headers["Authorization"] = token;
+    config.baseURL = URL;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export default axios;
