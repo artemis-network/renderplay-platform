84285ffa2544cbbf9bf6b0ef61e33e391f9ecbd5
artemis-network <artemis-network>
2022-07-07 11:49:13 +0530

    update

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@ let URL = "";
 if (SERVER === "DEV") URL = "http://localhost:5000";
 if (SERVER === "PROD") URL = "https://api.renderverse.io";
 
-const rendlePrefix = URL + "/backend/v1/rendles"
-const userPrefix = URL + "/backend/v1/users"
-const renderScanPrefix = URL + "/backend/v1/renderscans"
-const walletPrefix = URL + "/backend/v1/wallets"
+const rendlePrefix = URL + "/renderplay/v1/rendles"
+const userPrefix = URL + "/renderplay/v1/users"
+const renderScanPrefix = URL + "/renderplay/v1/renderscans"
+const walletPrefix = URL + "/renderplay/v1/wallets"
 
 const headers = () => {
 	return {
