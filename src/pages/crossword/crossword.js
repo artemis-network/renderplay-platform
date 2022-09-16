e81dd6f58ea5ff644d2c512d20a0bc89a4c8e113
akashmadduru <akashmadduru@gmail.com>
2022-09-16 16:43:45 +0530

    adding comments

--- src/pages/crossword/crossword.js
+++ src/pages/crossword/crossword.js
@@ -1,53 +1,97 @@
 import React from 'react'
 import Crossword from '@jaredreisinger/react-crossword';
 import Bar from '../common/bar/Bar';
+import './CrossWord.css'
 
 const data = {
 	across: {
 		1: {
-			clue: 'one plus one',
-			answer: 'TWO',
-			row: 1,
-			col: 0,
-		},
-	},
-	down: {
-		2: {
-			clue: '?',
-			answer: 'WHO',
-			row: 1,
+			clue: 'Question of self-doubt: 2wds',
+			answer: 'CANI',
+			row: 0,
 			col: 1,
 		},
-		3: {
-			clue: 'three minus two',
-			answer: 'ONE',
+		5: {
+			clue: 'Westren Canadian Territory',
+			answer: "YUKON",
 			row: 1,
+			col: 0
+		},
+		6: {
+			clue: "Martini add-in",
+			answer: "OLIVE",
+			row: 2,
+			col: 0
+		},
+		7: {
+			clue: "Speak",
+			answer: "UTTER",
+			row: 3,
+			col: 0
+		},
+		8: {
+			clue: "Condiment in shakers",
+			answer: "SALT",
+			row: 4,
+			col: 1
+		},
+
+	},
+	down: {
+		1: {
+			clue: 'Religious sects',
+			answer: 'CULT',
+			row: 0,
+			col: 1,
+		},
+		2: {
+			clue: 'Japanese dog',
+			answer: 'AKITA',
+			row: 0,
+			col: 2
+		},
+		3: {
+			clue: 'Book of fiction',
+			answer: 'NOVEL',
+			row: 0,
 			col: 2,
 		},
 		4: {
-			clue: 'birds builds',
-			answer: 'NEST',
-			row: 2,
-			col: 2,
+			clue: 'Motionless',
+			answer: 'INERT',
+			row: 0,
+			col: 4,
 		},
 		5: {
-			clue: 'persnolity trait',
-			answer: 'HONEST',
-			row: 0,
-			col: 2,
-
+			clue: 'Last word of happy birthday song',
+			answer: 'YOU',
+			row: 1,
+			col: 0,
 		},
+
+
 	},
 }
 const CrosswordComp = () => {
 	return (
-		<div style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column", background: "#" }}>
+		<div style={{ height: "100vh", background: '#321e43', width: "100vw", display: "flex", flexDirection: "column", alignContent: 'center' }}>
 			<Bar />
-
-			<div style={{ margin: "10vh 0", height: "30vh", width: "30vw", display: "flex", alignSelf: "center", justifyContent: 'center', background: "white" }}>
-				<Crossword data={data} />
+			<div style={{ padding: "5vh", columnGap: "3rem", height: "40vh", width: "50vw", display: "flex", flexDirection: "row", justifyContent: "center", alignSelf: "center", }}>
+				<Crossword
+					theme={{
+						cellBackground: "#6d1daf",
+						gridBackground: "#fbd6d2",
+						cellBorder: "#fbd6d2",
+						columnBreakpoint: "",
+						focusBackground: "#321e43",
+						highlightBackground: "#321e43",
+						numberColor: "#fbd6d2",
+						textColor: "#fbd6d2"
+					}}
+					data={data}>
+				</Crossword>
 			</div>
-		</div>
+		</div >
 
 	);
 }
