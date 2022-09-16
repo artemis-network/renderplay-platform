0424bff54658db384cb1253f9b82d4003408403d
akashmadduru <akashmadduru@gmail.com>
2022-09-16 23:21:06 +0530

    fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -21,28 +21,30 @@ const Home = () => {
 		"Maze", "Discover", "Zombie", "Drag Race",
 	]
 	function goToPage(item, index) {
-		if (item == 0) {
-			if (index == 1) {
+		console.log(item, index)
+		if (item == 1) {
+			if (index === 0) {
 				history.push("/rendle")
-			} else[
+			}
+			if (index === 1) [
 				history.push("/crossword")
 			]
 		}
 	}
-	return <div style={{ background: "#4C0033", fontFamily: "quicksand", padding: "5rem" }}>
-		<Box bgColor={"#790252"} display={"flex"} p={"8"} rowGap={"2rem"} flexDirection="column" borderRadius={"2xl"} color="#fbd6d2" >
+	return <div style={{ background: "#321e43", fontFamily: "quicksand", padding: "5rem" }}>
+		<Box bgColor={"#6d1daf"} display={"flex"} p={"8"} rowGap={"2rem"} flexDirection="column" borderRadius={"2xl"} color="#fbd6d2" >
 			<Flex flexDirection={"column"} py="4">
 				<Text fontSize={"3xl"} fontWeight="bold">Welcome to Renderverse</Text>
 				<Text fontWeight={"bold"}>Web3 Games, NFT, Community & Earnings</Text>
 			</Flex>
 			<Flex flexDirection={"column"}>
-				<Box bgColor={"#4C0033"} fontWeight="bold" p="3" borderTopRadius={"2xl"} width={"15%"} textAlign="center">
+				<Box bgColor={"#321e43"} fontWeight="bold" p="3" borderTopRadius={"2xl"} width={"15%"} textAlign="center">
 					<Text>Gameinfinity Web3 Games</Text>
 				</Box>
-				<Box bgColor={"#4C0033"} p="12" borderBottomRadius={"2xl"} borderTopRightRadius={"2xl"}>
+				<Box bgColor={"#321e43"} p="12" borderBottomRadius={"2xl"} borderTopRightRadius={"2xl"}>
 					<Grid templateColumns='repeat(5, 1fr)' gap={6}>
 						{gridItems.map((grid, index) =>
-							<GridItem onClick={() => goToPage(grid, index)} position={"relative"} key={index} w='100%' h='64' bg='#790252' borderRadius={"2xl"}>
+							<GridItem onClick={() => goToPage(grid, index)} position={"relative"} key={index} w='100%' h='64' bg='#6d1daf' borderRadius={"2xl"}>
 								{
 									gridGames[index] !== "" ? <Box display={"flex"} justifySelf="center" alignSelf={"center"}>
 										<Text fontWeight={"bold"} fontSize="3xl" p="4">{gridGames[index]}</Text>
@@ -50,7 +52,7 @@ const Home = () => {
 								}
 								{grid == 0 ?
 									<Box position={"absolute"} bottom="2" right={"2"}>
-										<Box bg="#4C0033" px={"3"} py="2" borderRadius={"2xl"}>
+										<Box bg="#321e43" px={"3"} py="2" borderRadius={"2xl"}>
 											<Text fontWeight={"bold"}>
 												Comming Soon
 											</Text>
@@ -59,7 +61,7 @@ const Home = () => {
 									: null}
 								{grid == 1 ?
 									<Box position={"absolute"} bottom="2" right={"2"}>
-										<Box bg="#4C0033" px={"3"} py="2" borderRadius={"2xl"}>
+										<Box bg="#321e43" px={"3"} py="2" borderRadius={"2xl"}>
 											<Text fontWeight={"bold"}>
 												Play Now
 											</Text>
